52b39cf4d3847fdda26736330ffdcc014832dd93
itsmefox <patrick.boesch@viascom.ch>
2022-04-06 16:30:12 +0200

    feat: add default emotes

--- src/main/kotlin/io/viascom/discord/bot/starter/bot/emotes/DefaultSystemEmoteLoader.kt
+++ src/main/kotlin/io/viascom/discord/bot/starter/bot/emotes/DefaultSystemEmoteLoader.kt
@@ -3,7 +3,7 @@ package io.viascom.discord.bot.starter.bot.emotes
 import net.dv8tion.jda.api.entities.Emoji
 
 class DefaultSystemEmoteLoader : SystemEmoteLoader {
-    override fun getCross(): Emoji = Emoji.fromMarkdown("")
+    override fun getCross(): Emoji = Emoji.fromMarkdown("❌")
 
-    override fun getCheck(): Emoji = Emoji.fromMarkdown("")
+    override fun getCheck(): Emoji = Emoji.fromMarkdown("✅")
 }
