5e2357e875222aaccf8e00a4ccd3569e91c897a7
itsmefox <patrick.boesch@viascom.ch>
2022-04-11 13:03:10 +0200

    fix: remove shitpack

--- src/main/kotlin/io/viascom/discord/bot/starter/botlist/TopGGBotListSender.kt
+++ src/main/kotlin/io/viascom/discord/bot/starter/botlist/TopGGBotListSender.kt
@@ -1,39 +1,49 @@
 package io.viascom.discord.bot.starter.botlist
 
+import com.google.gson.Gson
 import io.viascom.discord.bot.starter.property.AlunaProperties
 import net.dv8tion.jda.api.sharding.ShardManager
-import org.discordbots.api.client.DiscordBotListAPI
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class TopGGBotListSender(
     private val alunaProperties: AlunaProperties,
-    private val shardManager: ShardManager
+    private val shardManager: ShardManager,
+    private val gson: Gson
 ) : BotListSender {
-
-    private var topGGApi: DiscordBotListAPI? = null
 
     val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     override fun sendStats(totalServer: Int, totalShards: Int) {
         val topGGToken = alunaProperties.botList.topggToken
-        if(topGGToken == null){
+        if (topGGToken == null) {
             logger.debug("Stats are not sent to top.gg because token is not set")
             return
         }
 
         logger.debug("Send stats to top.gg")
 
-        if (topGGApi == null) {
-            val clientId = alunaProperties.discord.applicationId
-            topGGApi = DiscordBotListAPI.Builder()
-                .token(topGGToken)
-                .botId(clientId)
-                .build()
-        }
+        val httpClient = OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
 
-        topGGApi!!.setStats(shardManager.shards.map { it.guilds.size })
+        val request = Request.Builder().url("https://top.gg/api/bots/${alunaProperties.discord.applicationId}/stats").post(
+            RequestBody.create(MediaType.get("application/json"), gson.toJson(TopGGData(shardManager.shards.map { it.guilds.size })))
+        ).header("Authorization", topGGToken).build()
+
+        httpClient.newCall(request).execute()
     }
+
+    class TopGGData(
+        val shards: List<Int>
+    )
 }
