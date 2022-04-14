359a698050b13568242432d0f73061567fe5a661
itsmefox <patrick.boesch@viascom.ch>
2022-04-14 23:24:57 +0200

    fix: in scope loading of attributes chore: 0.0.14_5.0.0-alpha.9

---
+++ src/test/kotlin/io/viascom/discord/bot/starter/ExtendedCommand.kt
@@ -0,0 +1,5 @@
+package io.viascom.discord.bot.starter
+
+import io.viascom.discord.bot.starter.bot.handler.DiscordCommand
+
+abstract class ExtendedCommand(name: String, description: String, observeAutoComplete: Boolean = false) : DiscordCommand(name, description, observeAutoComplete)
