aaee1b1a4e2e6ea6ebe294e1d6a10002be9ab05a
itsmefox <patrick.boesch@viascom.ch>
2022-06-25 12:14:42 +0200

    feat: add options to constructor of CommandOptions

--- src/main/kotlin/io/viascom/discord/bot/aluna/model/Option.kt
+++ src/main/kotlin/io/viascom/discord/bot/aluna/model/Option.kt
@@ -3,39 +3,95 @@
 
 package io.viascom.discord.bot.aluna.model
 
-import net.dv8tion.jda.api.entities.Channel
-import net.dv8tion.jda.api.entities.IMentionable
+import net.dv8tion.jda.api.entities.*
 import net.dv8tion.jda.api.entities.Message.Attachment
-import net.dv8tion.jda.api.entities.Role
-import net.dv8tion.jda.api.entities.User
+import net.dv8tion.jda.api.interactions.commands.Command.Choice
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
 interface CommandOption<T>
 
-class StringOption(name: String, description: String, isRequired: Boolean = false, isAutoComplete: Boolean = false) :
-    OptionData(OptionType.STRING, name, description, isRequired, isAutoComplete), CommandOption<String?>
+class OptionRange<T : Any>(val min: T, val max: T)
 
-class IntegerOption(name: String, description: String, isRequired: Boolean = false, isAutoComplete: Boolean = false) :
-    OptionData(OptionType.INTEGER, name, description, isRequired, isAutoComplete), CommandOption<Int?>
+class StringOption @JvmOverloads constructor(
+    name: String,
+    description: String,
+    isRequired: Boolean = false,
+    isAutoComplete: Boolean = false,
+    choices: Map<String, String>? = null
+) :
+    OptionData(OptionType.STRING, name, description, isRequired, isAutoComplete), CommandOption<String?> {
+    init {
+        choices?.let { this.addChoices(it.entries.map { entry -> Choice(entry.key, entry.value) }) }
+    }
+}
 
-class NumberOption(name: String, description: String, isRequired: Boolean = false, isAutoComplete: Boolean = false) :
-    OptionData(OptionType.NUMBER, name, description, isRequired, isAutoComplete), CommandOption<Double?>
+class IntegerOption @JvmOverloads constructor(
+    name: String,
+    description: String,
+    isRequired: Boolean = false,
+    isAutoComplete: Boolean = false,
+    choices: Map<String, Int>? = null,
+    requiredRange: OptionRange<Int>? = null
+) :
+    OptionData(OptionType.INTEGER, name, description, isRequired, isAutoComplete), CommandOption<Int?> {
+    init {
+        choices?.let { this.addChoices(it.entries.map { entry -> Choice(entry.key, entry.value.toLong()) }) }
+        requiredRange?.let { this.setRequiredRange(requiredRange.min.toLong(), requiredRange.max.toLong()) }
+    }
+}
 
-class BooleanOption(name: String, description: String, isRequired: Boolean = false) :
+class LongOption @JvmOverloads constructor(
+    name: String,
+    description: String,
+    isRequired: Boolean = false,
+    isAutoComplete: Boolean = false,
+    choices: Map<String, Long>? = null,
+    requiredRange: OptionRange<Long>? = null
+) :
+    OptionData(OptionType.INTEGER, name, description, isRequired, isAutoComplete), CommandOption<Long?> {
+    init {
+        choices?.let { this.addChoices(it.entries.map { entry -> Choice(entry.key, entry.value) }) }
+        requiredRange?.let { this.setRequiredRange(requiredRange.min, requiredRange.max) }
+    }
+}
+
+class DoubleOption @JvmOverloads constructor(
+    name: String,
+    description: String,
+    isRequired: Boolean = false,
+    isAutoComplete: Boolean = false,
+    choices: Map<String, Double>? = null,
+    requiredRange: OptionRange<Double>? = null
+) :
+    OptionData(OptionType.NUMBER, name, description, isRequired, isAutoComplete), CommandOption<Double?> {
+    init {
+        choices?.let { this.addChoices(it.entries.map { entry -> Choice(entry.key, entry.value) }) }
+        requiredRange?.let { this.setRequiredRange(requiredRange.min, requiredRange.max) }
+    }
+}
+typealias NumberOption = DoubleOption
+
+class BooleanOption @JvmOverloads constructor(name: String, description: String, isRequired: Boolean = false) :
     OptionData(OptionType.BOOLEAN, name, description, isRequired), CommandOption<Boolean?>
 
-class UserOption(name: String, description: String, isRequired: Boolean = false) :
+class UserOption @JvmOverloads constructor(name: String, description: String, isRequired: Boolean = false) :
     OptionData(OptionType.USER, name, description, isRequired), CommandOption<User?>
 
-class RoleOption(name: String, description: String, isRequired: Boolean = false) :
+class RoleOption @JvmOverloads constructor(name: String, description: String, isRequired: Boolean = false) :
     OptionData(OptionType.ROLE, name, description, isRequired), CommandOption<Role?>
 
-class ChannelOption(name: String, description: String, isRequired: Boolean = false) :
-    OptionData(OptionType.CHANNEL, name, description, isRequired), CommandOption<Channel?>
+class ChannelOption @JvmOverloads constructor(name: String, description: String, isRequired: Boolean = false, channelTypes: List<ChannelType>? = null) :
+    OptionData(OptionType.CHANNEL, name, description, isRequired), CommandOption<Channel?> {
+    init {
+        if (channelTypes != null) {
+            this.setChannelTypes(channelTypes)
+        }
+    }
+}
 
-class MentionableOption(name: String, description: String, isRequired: Boolean = false) :
+class MentionableOption @JvmOverloads constructor(name: String, description: String, isRequired: Boolean = false) :
     OptionData(OptionType.MENTIONABLE, name, description, isRequired), CommandOption<IMentionable?>
 
-class AttachmentOption(name: String, description: String, isRequired: Boolean = false) :
+class AttachmentOption @JvmOverloads constructor(name: String, description: String, isRequired: Boolean = false) :
     OptionData(OptionType.ATTACHMENT, name, description, isRequired), CommandOption<Attachment?>
--- src/test/kotlin/io/viascom/discord/bot/aluna/PingCommand.kt
+++ src/test/kotlin/io/viascom/discord/bot/aluna/PingCommand.kt
@@ -4,13 +4,16 @@ import io.viascom.discord.bot.aluna.bot.Command
 import io.viascom.discord.bot.aluna.bot.DiscordCommand
 import io.viascom.discord.bot.aluna.bot.listener.EventWaiter
 import io.viascom.discord.bot.aluna.model.ChannelOption
+import io.viascom.discord.bot.aluna.model.DoubleOption
+import io.viascom.discord.bot.aluna.model.OptionRange
 import io.viascom.discord.bot.aluna.model.StringOption
-import io.viascom.discord.bot.aluna.util.*
+import io.viascom.discord.bot.aluna.util.createPrimaryButton
+import io.viascom.discord.bot.aluna.util.getTypedOption
+import io.viascom.discord.bot.aluna.util.removeActionRows
+import net.dv8tion.jda.api.entities.ChannelType
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.OptionMapping
-import net.dv8tion.jda.api.interactions.commands.OptionType
-import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import net.dv8tion.jda.api.interactions.components.ActionRow
 import net.dv8tion.jda.api.sharding.ShardManager
 import java.time.Duration
@@ -30,16 +33,18 @@ class PingCommand(
     }
 
     private val mapOption = StringOption("map", "Select a map", isRequired = false, isAutoComplete = true)
-    private val channelOption = ChannelOption("channel", "Select a map", isRequired = false)
+    private val channelOption = ChannelOption("channel", "Select a map", isRequired = false, arrayListOf(ChannelType.VOICE))
+    private val rangeOption = DoubleOption("range", "Select a map", isRequired = false, isAutoComplete = false, requiredRange = OptionRange(1.2, 10.0))
 
     override fun initCommandOptions() {
-        this.addOptions(mapOption, channelOption)
+        this.addOptions(mapOption, channelOption, rangeOption)
     }
 
     override fun execute(event: SlashCommandInteractionEvent) {
         logger.debug("command: " + this.hashCode().toString())
         val selectedMap = event.getTypedOption(mapOption)
         val selectedChannel = event.getTypedOption(channelOption, OptionMapping::getAsAudioChannel)
+        val range = event.getTypedOption(rangeOption, 1.2)!!
         val oldWayChannel = event.getOption("channel")
 
         event.reply("Pong\nYour locale is:${this.userLocale}").addActionRows(ActionRow.of(createPrimaryButton("hi", "Hi"))).queue {
@@ -59,7 +64,6 @@ class PingCommand(
 //        logger.debug("autocomplete: " + this.hashCode().toString())
 //        event.replyChoice("hello", "hello").queue()
 //    }
-
 
 
     override fun onDestroy() {
